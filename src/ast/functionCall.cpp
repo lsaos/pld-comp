a3035590ec326c91402a5a8dfd619a2bcacce394
lsaos <loicsaos@gmail.com>
2019-03-29 10:41:44 +0100

    Array variables

--- src/ast/functionCall.cpp
+++ src/ast/functionCall.cpp
@@ -5,6 +5,8 @@
 //
 
 #include "functionCall.hpp"
+#include "function.hpp"
+#include "variable.hpp"
 
 namespace ast
 {
@@ -37,6 +39,25 @@ namespace ast
 		return identifier->getType();
 	}
 
+	Function* FunctionCall::getCalledFunction() const
+	{
+		if (identifier) {
+			return identifier->getReferencedFunction();
+		}
+		else {
+			return nullptr;
+		}
+	}
+
+	vector<Expression*> FunctionCall::getArguments() const
+	{
+		vector<Expression*> ar;
+		for (const auto& arg : args) {
+			ar.push_back(arg.get());
+		}
+		return ar;
+	}
+
 	void FunctionCall::checkSemantic(bool advanced) const
 	{
 		if (!identifier) {
@@ -46,8 +67,26 @@ namespace ast
 		identifier->checkSemantic(advanced);
 
 		// The identifier must reference an existing function
-		if (!identifier->isReferencingFunction()) {
-			error(Error::InvalidStatement, identifier.get());
+		const Function* func = identifier->getReferencedFunction();
+		if (!func) {
+			error(Error::UnknownIdentifier, identifier.get());
+		}
+
+		const vector<Variable*> params = func->getParameters();
+		if (args.size() != params.size()) {
+			error(Error::WrongArgumentsCount, this);
+		}
+
+		for (size_t i = 0; i < args.size(); i++) {
+			args[i]->checkSemantic(advanced);
+
+			if (args[i]->getType() == Type::Void) {
+				error(Error::InvalidStatement, args[i].get());
+			}
+
+			if (advanced && i < params.size() && !args[i]->isCastSafe(params[i]->getType())) {
+				warning(Warning::UnsafeCast, args[i].get());
+			}
 		}
 	}
 
@@ -59,6 +98,16 @@ namespace ast
 
 		for (auto& arg : args) {
 			arg->prepare();
+		}
+	}
+
+	string FunctionCall::getStringRepresentation() const
+	{
+		if (identifier) {
+			return identifier->getIdent();
+		}
+		else {
+			return string();
 		}
 	}
 }
--- src/ast/variable.cpp
+++ src/ast/variable.cpp
@@ -29,6 +29,28 @@ namespace ast
 		used = true;
 	}
 
+	void Variable::setIsArray()
+	{
+		isTypeArray = true;
+	}
+
+	void Variable::setArraySize(Expression* expr)
+	{
+		assert(expr);
+		expr->setParent(this);
+		arraySize = unique_ptr<Expression>(expr);
+	}
+
+	int Variable::getArraySize() const
+	{
+		if (arraySize && arraySize->isConstant()) {
+			return arraySize->getValue();
+		}
+		else {
+			return -1;
+		}
+	}
+
 	void Variable::checkSemantic(bool advanced) const
 	{
 		if (getName().empty() || getType() == Type::Void) {
@@ -38,6 +60,49 @@ namespace ast
 		if (advanced && !used) {
 			warning(Warning::UnusedVariable, this);
 		}
+
+		if (isTypeArray && scope != Scope::Parameter) {
+			if (!arraySize) {
+				error(Error::MissingArraySize, this);
+			}
+
+			arraySize->checkSemantic(advanced);
+
+			if (arraySize->getType() == Type::Void || !arraySize->isConstant()) {
+				error(Error::NotConstant, arraySize.get());
+			}
+
+			if (arraySize->getValue() < 0) {
+				error(Error::InvalidArraySize, arraySize.get());
+			}
+		}
+		else {
+			if (arraySize) {
+				error(Error::InvalidStatement, arraySize.get());
+			}
+		}
+	}
+
+	Instruction* Variable::optimize()
+	{
+		// Try to optimize the array size expression
+		if (arraySize) {
+			Instruction* optimized = arraySize->optimize();
+			if (optimized) {
+				assert(optimized != arraySize.get() && optimized->isExpression());
+				arraySize = unique_ptr<Expression>((Expression*)optimized);
+			}
+		}
+
+		// We can't optimize the variable itself
+		return nullptr;
+	}
+
+	void Variable::prepare()
+	{
+		if (arraySize) {
+			arraySize->prepare();
+		}
 	}
 
 	void Variable::toTextualRepresentation(ostream& out, size_t i) const
@@ -46,10 +111,22 @@ namespace ast
 		out << "Var {" << endl;
 
 		for (size_t j = 0; j < i + 1; j++) { out << ' '; }
-		out << getTypeName() << endl;
+		out << getTypeName();
+		if (isTypeArray) {
+			out << "[]";
+		}
+		out << endl;
 
 		for (size_t j = 0; j < i + 1; j++) { out << ' '; }
 		out << "Ident(" << getName() << ')' << endl;
+
+		if (isTypeArray) {
+			const int size = getArraySize();
+			if (size >= 0) {
+				for (size_t j = 0; j < i + 1; j++) { out << ' '; }
+				out << "Size(" << size << ')' << endl;
+			}
+		}
 
 		for (size_t j = 0; j < i; j++) { out << ' '; }
 		out << '}' << endl;
@@ -57,7 +134,20 @@ namespace ast
 
 	string Variable::getStringRepresentation() const
 	{
-		return getTypeName() + ' ' + getName();
+		string s = getTypeName() + ' ' + getName();
+
+		if (isTypeArray) {
+			s += '[';
+
+			const int size = getArraySize();
+			if (size >= 0) {
+				s += to_string(size);
+			}
+
+			s += ']';
+		}
+
+		return s;
 	}
 
 	/*void Variable::generateAssembly(ofstream& f, unordered_map<ast::Variable*, int>& addressTable)
