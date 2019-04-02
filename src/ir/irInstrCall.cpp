63edc82933ed9415e647a392e83a8aa0fbf8095d
Alexandra <alex0317@hotmail.fr>
2019-04-02 15:53:45 +0200

    màj des ir instructions + add copy

--- src/ir/irInstrCall.cpp
+++ src/ir/irInstrCall.cpp
@@ -1,16 +1,37 @@
 #pragma once
 
 #include "irInstrCall.hpp"
+#include "basicBlock.hpp"
+#include "cfg.hpp"
 
 using namespace std;
 using namespace ast;
 using namespace ir;
 
-IRInstrCall::IRInstrCall(BasicBlock* bb, string dest, string label, vector <string> op) : IRInstr(bb) { 
+IRInstrCall::IRInstrCall(BasicBlock* bb, string dest, string label, vector <string> op) : IRInstr(bb, (bb->get_cfg())->get_var_type(dest)) { 
 	this->dest = dest;
 	this->label = label;
-	//op(op.begin(), op.end()); //to check
+	for (string s : op) {
+		op.push_back(s);
+	}
 }
 
 void IRInstrCall::gen_asm(ostream &o) {} // TODO
 
+/*f(2,3);
+movl $3, %esi
+movl $2, %edi
+call f(int, int)*/
+
+
+
+void IRInstrCall::printIR(ostream &o) {
+	o << "\tcall " << dest << " " << label << " (";
+	for (auto i : op) {
+		if (i != op[0]) {
+			o << ",";
+		}
+		o << " i";
+	}
+	o << " )" << endl;
+}
--- src/ir/irInstrRmen.cpp
+++ src/ir/irInstrRmen.cpp
@@ -1,11 +1,34 @@
 #pragma once
 
 #include "irInstrRmen.hpp"
+#include "basicBlock.hpp"
+#include "cfg.hpp"
 
 using namespace std;
 using namespace ast;
 using namespace ir;
 
-IRInstrRmen::IRInstrRmen(BasicBlock* bb, string addr, string dest) : IRInstr(bb), addr(addr), dest(dest) { }
+IRInstrRmen::IRInstrRmen(BasicBlock* bb, string addr, string dest) : IRInstr(bb, (bb->get_cfg())->get_var_type(addr)), addr(addr), dest(dest) { }
 
-void IRInstrRmen::gen_asm(ostream &o) {} // TODO
+void IRInstrRmen::gen_asm(ostream &o) {
+	// check the variable type
+	string TYPE;
+	switch (t){
+		case Type::Integer:
+			TYPE = "l";
+			break;
+		case Type::Character:
+			TYPE = "b";
+			break;
+	}
+
+	//write the assembly code : the content of address addr is copied in variable dest
+	o << "\tmov" << TYPE << " -" << dest.substr(4) << "(%rbp), %eax" << endl;
+	o << "\tmov" << TYPE << " -" << addr.substr(4) << "(%rbd), %r10" << endl;
+	o << "\tmov" << TYPE << " %r10, (%eax)" << endl;
+}
+
+void IRInstrRmen::printIR(ostream &o) {
+	o << "\trmen " << dest << " " << addr << endl;
+}
+
