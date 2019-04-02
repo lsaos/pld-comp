92946dafcf74e1808a3f68efee4a069f4d425b0a
Alexandra <alex0317@hotmail.fr>
2019-04-02 10:14:23 +0200

    Modification de la classe IRInstr (virtual) + ajout des classes pour Rmen, Wmen et Call

---
+++ src/ir/irInstrRmen.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "irInstr.hpp"
+
+using namespace std;
+using namespace ast;
+
+namespace ir {
+
+	class IRInstrRmen : public virtual IRInstr {
+
+		public:
+			/**  constructor */
+			IRInstrRmen(BasicBlock* bb, string addr, string dest);
+
+			/** Actual code generation */
+			void gen_asm(ostream &o);
+
+		protected:
+			string addr;
+			string dest;
+	};
+}
