f19685b9e65ac985d59a3ff6b0332309cc63ae7b
manahid <aminenahid@gmail.com>
2019-04-04 00:08:19 +0200

    Corrections méthodes assembleur et ajout de code génération langage IR

--- src/ir/irInstrBinaryOperation.cpp
+++ src/ir/irInstrBinaryOperation.cpp
@@ -16,6 +16,7 @@ void IRInstrBinaryOperation::gen_asm(ostream &o)
 {
 	// check the variable type
 	string type = "l";
+	string action = "add";
 
 	switch (t) {
 	case Type::Integer:
@@ -26,8 +27,40 @@ void IRInstrBinaryOperation::gen_asm(ostream &o)
 		break;
 	}
 
+	switch (operation)
+	{
+	case Operation::add:
+		action = "add";
+		break;
+	case Operation::sub:
+		action = "sub";
+		break;
+	case Operation::mul:
+		action = "imul";
+		break;
+	}
+
 	//Assembler's code generation 
 	o << "\tmov" << type << " " << bb->get_cfg()->get_var_index(operand1) << "(%rbp), %eax" << endl;
-	o << "\tadd" << type << " " << bb->get_cfg()->get_var_index(operand2) << "(%rbp), %eax" << endl;
+	o << "\t" << action << type << " " << bb->get_cfg()->get_var_index(operand2) << "(%rbp), %eax" << endl;
 	o << "\tmov" << type << " " << bb->get_cfg()->get_var_index(operand2) << "(%rbp), "<< bb->get_cfg()->get_var_index(destination) << "(%rbp)" << endl;
+}
+
+void IRInstrBinaryOperation::printIR(ostream &o)
+{
+	string mnemonic = "add";
+	switch (operation)
+	{
+	case Operation::add:
+		mnemonic = "add";
+		break;
+	case Operation::sub:
+		mnemonic = "sub";
+		break;
+	case Operation::mul:
+		mnemonic = "mul";
+		break;
+	}
+
+	o << "\t" << mnemonic << " " << destination << " " << operand1 << " " << operand2 << endl;
 }
