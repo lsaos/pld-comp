f211fcf6674bab45f9788c9ca0832d2d28b78817
aurelienbelin <aurelienbelin@ABELIN.localdomain>
2019-04-04 23:26:22 +0200

    Implémentation des instructions IR et de la génération assembleur associée pour les UnaryExpressions

---
+++ src/ir/irInstrUnaryOperation.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "irInstr.hpp"
+
+using namespace std;
+using namespace ast;
+
+namespace ir {
+
+	class IRInstrUnaryOperation : public virtual IRInstr {
+
+	public:
+
+		typedef enum {
+			minus,
+			logicalNot
+		} UnaryOperation;
+
+		/**  constructor */
+		//IRInstr(BasicBlock*, Type t);
+		IRInstrUnaryOperation(BasicBlock*, UnaryOperation op, string dest, string operand);
+
+		/** Actual code generation */
+		void gen_asm(ostream &o); /**< x86 assembly code generation for this IR instruction */
+
+		void printIR(ostream &o);
+
+	protected:
+		string destination;
+		string operand;
+		UnaryOperation operation;
+
+	};
+}
