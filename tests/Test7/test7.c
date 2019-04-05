55ffd8b95776a949c1bce8a49e8c74bb385946da
lsaos <loicsaos@gmail.com>
2019-04-05 11:03:19 +0200

    Fix function nested blocks

--- tests/Test7/test7.c
+++ tests/Test7/test7.c
@@ -1,10 +1,4 @@
-int bonjour(int a, int b)
-{
-	int c = a + b;
-	return 2 *c;
-}
-
 int main()
 {
-	return bonjour(1, 1);
+	return 1+2;
 }
--- tests/Test7/test7.s
+++ tests/Test7/test7.s
@@ -4,11 +4,11 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	popq %rbp
-	ret
-
-bonjour:
-	pushq %rbp
-	movq %rsp, %rbp
+	movl $1, -4(%rbp)
+	movl $2, -8(%rbp)
+	movl -4(%rbp), %eax
+	addl -8(%rbp), %eax
+	movl %eax, -12(%rbp)
+	movl -12(%rbp), %eax
 	popq %rbp
 	ret
