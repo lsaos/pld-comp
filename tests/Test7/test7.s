6b001a8687a396b73e942d69c5a768cb8174f11e
lsaos <loicsaos@gmail.com>
2019-04-05 11:55:48 +0200

    Array index textual representation

--- tests/Test7/test7.s
+++ tests/Test7/test7.s
@@ -4,7 +4,10 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	movl $3, -4(%rbp)
+	movl $2, -8(%rbp)
 	movl -4(%rbp), %eax
+	addl -8(%rbp), %eax
+	movl %eax, -12(%rbp)
+	movl -12(%rbp), %eax
 	popq %rbp
 	ret
